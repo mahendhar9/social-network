80c2b40e0b706fad8db611275e97e5c4f5701072
Mahendhar <mahendarreddy@live.com>
2016-02-18 21:14:00 +0530

    Users can now filter other users based on their Friendship type with them

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
   before_action :find_user, only: [:show]
+  before_action :get_counts, only: [:index]
 
   def index
-    @users = User.all
+    case params[:show] 
+    when "friends"
+      @users = current_user.active_friends
+    when "requested"
+      @users = current_user.pending_sent_requests.map(&:friend)
+    when "pending"
+      @users = current_user.pending_received_requests.map(&:user)
+    else
+      @users = User.where.not(id: current_user.id)
+    end
   end
 
   def show
@@ -14,5 +24,8 @@ class UsersController < ApplicationController
     @user = User.find_by(username: params[:id])
   end
 
-
+  def get_counts
+    @friends_count = current_user.active_friends.size
+    @pending_sent_requests_count = current_user.pending_sent_requests.map(&:friend).size
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def pending_received_requests_count
+    current_user.pending_received_requests.map(&:user).size
+  end
 end
